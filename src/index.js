9ee3ab60f8310e4b447641c6ceb2eae6411b3254
flowerpower888 <oingooboingo@gmail.com>
2021-02-09 14:22:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = reverse = (n) => +[...n.toString()]
+        .filter(x => +x == x)
+        .reverse()
+        .reduce((acc, cur) => acc + cur)
